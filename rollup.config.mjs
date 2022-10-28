09db56a4bb38c191e37a1ba2e05aa9164bb73df9
mikeabbott10 <amorelli.lorenzo@gmail.com>
2022-10-28 16:16:49 +0200

    rollup config fixed

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@el3um4s/rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 //import * as packageJson from './package.json' assert { type: "json" };
